687a55676acddcebde504500ccb2d5052afdc39f
Konler <yulya201195@mail.ru>
2023-06-14 04:48:50 +0300

    На ревью

--- src/main/java/ru/practicum/shareit/PageSetup.java
+++ src/main/java/ru/practicum/shareit/PageSetup.java
@@ -3,6 +3,7 @@ package ru.practicum.shareit;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 public class PageSetup extends PageRequest {
+
     public PageSetup(Integer from, Integer size, Sort sort) {
         super(from / size, size, sort);
     }
